a5177a9ca7c875fb5d87b8539452ff946f218751
Jeremiah Orem <oremj@oremj.com>
2015-04-10 14:13:45 -0700

    Rename to autopush-supervisor

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 
 setup(
-    name='pushgo-supervisor',
-    version='0.0.6',
+    name='autopush-supervisor',
+    version='1.0.0',
     description='Monitors health of instances within an ELB '
                 'and adjusts a route53 resource group.',
     author='Jeremy Orem',
     author_email='oremj@mozilla.com',
-    scripts=['bin/pushgo-supervise',
+    scripts=['bin/autopush-supervisor',
              'bin/pushgo-cloudwatch-metrics'],
 )
